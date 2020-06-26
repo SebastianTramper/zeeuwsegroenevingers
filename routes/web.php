9c6472f0099f79028b56b45d72478412b705d1db
Sebastian Tramper <tramper.s@outlook.com>
2020-06-26 07:25:30 +0200

    regsitery

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,21 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::middleware('auth')->group(function() {
+    Route::get('/create', 'PostsControllers@create')->name('create');
+    Route::post('/create', 'PostsControllers@store')->name('store');
 
-Route::get('/create', 'PostsControllers@create')->name('create');
-Route::post('/create', 'PostsControllers@store')->name('store');
+    Route::get('/categorie', 'CategoriesController@index')->name('categorie');
 
-Route::get('/categorie', 'CategoriesController@index')->name('categorie');
+    Route::get('/categorie/new', 'CategoriesController@create')->name('categorie-create');
+    Route::post('/categorie', 'CategoriesController@store')->name('categorie-store');
 
-Route::get('/categorie/new', 'CategoriesController@create')->name('categorie-create');
-Route::post('/categorie', 'CategoriesController@store')->name('categorie-store');
+    Route::get('/categorie/edit', 'CategoriesController@edit')->name('categorie-edit');
+    Route::put('/categorie/edit/{categories}', 'CategoriesController@update')->name('categorie-update');
+    Route::delete('/categorie/delete/{categories}', 'CategoriesController@destroy')->name('categorie-delete');
 
+});
 
-Route::get('/categorie/edit', 'CategoriesController@edit')->name('categorie-edit');
-Route::put('/categorie/edit/{categories}', 'CategoriesController@update')->name('categorie-update');
-Route::delete('/categorie/delete/{categories}', 'CategoriesController@destroy')->name('categorie-delete');
 Route::get('/categorie/{categories}', 'CategoriesController@show')->name('categorie-show');
 
 Route::get('/{categories}', 'CategoriesController@index')->name('categorie');
