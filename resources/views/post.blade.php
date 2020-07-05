d41ee7f87110f795da8e8fee03e3311cd5964f72
Sebastian Tramper <tramper.s@outlook.com>
2020-07-05 20:50:31 +0200

    escape html

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+    <?php
+
+    ?>
 <div class="container col-md-8 py-5">
     <h1 class="h1">
         {{ $artikel["title"] }}
     </h1>
     <img src="/storage/{{ $artikel->image }}" class="img-fluid" style="max-height: 400px;width: 100%;object-fit: cover" alt="">
     <p class="mt-3">
-        {{ $artikel->text }}
+        {{!! $artikel->text !!}}
     </p>
 </div>
 @endsection
