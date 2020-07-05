a8be14f2daa317b72ff06ed1ab63d7d5aa88b173
Sebastian Tramper <tramper.s@outlook.com>
2020-07-05 21:35:43 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="icon" href="{{ asset('banner.jpeg') }}" type="image/png">
+    <link rel="icon" href="https://zeeuwsegroenevingers.nl/img/banner.jpeg" type="image/png">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
