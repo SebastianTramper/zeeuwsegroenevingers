3485016b148a61ff2c6beab60f73f993b91eb8e5
Sebastian Tramper <tramper.s@outlook.com>
2020-07-06 12:19:46 +0200

    changed styles header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,66 +32,71 @@
 
 </head>
 <body>
-    <div id="app" class="container">
-        <nav class="navbar navbar-expand-md navbar-light bg-white d-block">
-                <h1>
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Zeeuwse <span>G</span>roene vingers
-                    </a>
-                </h1>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app" >
+    <div class="container bg-white">
+        <h1 class="m-0 p-3 header-title">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Zeeuwse <span>G</span>roene vingers
+            </a>
+        </h1>
+    </div>
+    <div class="container p-0">
+        <section class="banner w-100" style="background: url( {{asset('img/banner.jpeg')}} ) center/cover;"></section>
+    </div>
+    <div class="container p-0">
+        <nav class="navbar bg-white navbar-expand-md d-block">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li><a href="/">home</a></li>
-                    </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    <li><a href="/">home</a></li>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
 
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a href="/create" class="dropdown-item">Nieuwe post</a>
+                                <a href="/categorie" class="dropdown-item">categorie instellingen</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a href="/create" class="dropdown-item">Nieuwe post</a>
-                                    <a href="/categorie" class="dropdown-item">categorie instellingen</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
         </nav>
-        <section class="banner w-100" style="background: url( {{asset('img/banner.jpeg')}} ) center/cover;">
-        </section>
-        <main class="pt-5">
-            @yield('content')
-        </main>
-        <footer class="w-100 position-relative mt-5">
-
-        </footer>
     </div>
+    <main class="pt-5 bg-white container">
+        @yield('content')
+    </main>
+    <footer class="w-100 container position-relative mt-5">
+
+    </footer>
+</div>
 
 
 </body>
